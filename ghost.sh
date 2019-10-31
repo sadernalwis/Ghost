7a5ecd5b9e519283cb45dbeabcaf3fa3265d1384
Sadern Alwis <44372160+sadernalwis@users.noreply.github.com>
2019-10-31 06:23:43 -0700

    Update ghost.sh

--- ghost.sh
+++ ghost.sh
@@ -1,3 +1,4 @@
 kstart --desktop 3 chromium-browser
 kstart --desktop 3 konsole -geometry 600x400+0+0
 
+kstart --alldesktops --keepbelow --skippager --skiptaskbar Eterm --geometry 90x30+365+315 --trans=true --borderless --scrollbar=false --buttonbar=false -f white --cmod 99 & 
