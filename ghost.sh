a3d7dc57444c22d69d58c945e89ec7496fe9e60c
Sadern Alwis <44372160+sadernalwis@users.noreply.github.com>
2020-01-15 00:43:48 +0530

    Update ghost.sh

--- ghost.sh
+++ ghost.sh
@@ -2,3 +2,9 @@ kstart --desktop 3 chromium-browser
 kstart --desktop 3 konsole -geometry 600x400+0+0
 
 kstart --alldesktops --keepbelow --skippager --skiptaskbar Eterm --geometry 90x30+365+315 --trans=true --borderless --scrollbar=false --buttonbar=false -f white --cmod 99 & 
+
+sudo add-apt-repository universe
+sudo add-apt-repository multiverse
+sudo add-apt-repository ppa:paulo-miguel-dias/mesa
+sudo apt-get update
+sudo apt update
